5d186d3205974a27fbe908e0789d8d7b84ab7fc9
Xiu-zhe (Roger) Luo <rogerluo.rl18@gmail.com>
2024-11-06 10:22:08 -0500

    simplify worklist (#81)

--- src/kirin/worklist.py
+++ src/kirin/worklist.py
@@ -13,42 +13,19 @@ class WorkList(Generic[ElemType]):
     The worklist is a stack that allows for O(1) removal of elements from the stack.
     """
 
-    # this is from XDSL
-    _stack: list[ElemType | None] = field(default_factory=list, init=False)
-    _map: dict[ElemType, int] = field(default_factory=dict, init=False)
-    # allow O(1) removal from the stack via storage of the index
-
-    def __contains__(self, item: ElemType) -> bool:
-        return item in self._map
-
-    def purge(self) -> None:
-        """remove all None values from the stack"""
-        while self._stack and self._stack[-1] is None:
-            self._stack.pop()
+    _stack: list[ElemType] = field(default_factory=list, init=False)
 
     def is_empty(self) -> bool:
-        self.purge()
         return len(self._stack) == 0
 
     def push(self, item: ElemType) -> None:
-        if item not in self._map:
-            self._map[item] = len(self._stack)
-            self._stack.append(item)
+        self._stack.append(item)
 
     def append(self, items: Iterable[ElemType]) -> None:
         for item in items:
             self.push(item)
 
     def pop(self) -> ElemType | None:
-        while self._stack:
-            item = self._stack.pop()
-            if item is not None:
-                del self._map[item]
-                return item
+        if self._stack:
+            return self._stack.pop()
         return None
-
-    def remove(self, item: ElemType) -> None:
-        if item in self._map:
-            idx = self._map[item]
-            self._stack[idx] = None
-            del self._map[item]
