28b1c194df0301e0bc18a5c7b1c162636a46e101
Roger-luo <rogerluo.rl18@gmail.com>
2024-11-06 11:11:54 -0500

    add docstring for graph methods

--- src/kirin/graph.py
+++ src/kirin/graph.py
@@ -8,10 +8,25 @@ Node = TypeVar("Node")
 
 
 class Graph(Protocol, Generic[Node]):
+    """The graph interface.
 
-    def get_neighbors(self, node: Node) -> Iterable[Node]: ...
-    def get_nodes(self) -> Iterable[Node]: ...
-    def get_edges(self) -> Iterable[tuple[Node, Node]]: ...
+    This interface defines the methods that a graph object must implement.
+    The graph interface is mainly for compatibility reasons so that one can
+    use multiple graph implementations interchangeably.
+    """
+
+    def get_neighbors(self, node: Node) -> Iterable[Node]:
+        """Get the neighbors of a node."""
+        ...
+
+    def get_nodes(self) -> Iterable[Node]:
+        """Get all the nodes in the graph."""
+        ...
+
+    def get_edges(self) -> Iterable[tuple[Node, Node]]:
+        """Get all the edges in the graph."""
+        ...
+
     def print(
         self,
         printer: Optional["Printer"] = None,
