0a527745a85d281d6289d2257d0d08cac76af873
Roger-luo <rogerluo.rl18@gmail.com>
2024-11-12 13:47:43 -0500

    alternative_prefix -> prefix_if_noen

--- src/kirin/idtable.py
+++ src/kirin/idtable.py
@@ -32,7 +32,7 @@ class IdTable(Generic[T]):
     """The count of names that have been generated."""
     next_id: int = 0
     """The next ID to use for generating names."""
-    alternate_prefix: str = ""
+    prefix_if_none: str = ""
     """An alternate prefix to use when the name is None."""
 
     def add(self, value: T) -> str:
@@ -45,7 +45,7 @@ class IdTable(Generic[T]):
             name = self.prefix + value_name + suffix
             self.table[value] = name
         else:
-            name = f"{self.prefix}{self.alternate_prefix}{id}"
+            name = f"{self.prefix}{self.prefix_if_none}{id}"
             self.next_id += 1
             self.table[value] = name
         return name
