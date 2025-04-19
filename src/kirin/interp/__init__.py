ccd5ad6e06422c959b0b3c6a14484222cff372c3
Roger-luo <rogerluo.rl18@gmail.com>
2025-04-19 00:26:34 -0400

    fix: cleanup interp module import

--- src/kirin/interp/__init__.py
+++ src/kirin/interp/__init__.py
@@ -15,7 +15,6 @@ abstract interpreters:
 - [`MethodTable`][kirin.interp.MethodTable]: Method table for registering implementations of statements.
 """
 
-from . import result as result
 from .base import BaseInterpreter as BaseInterpreter
 from .impl import ImplDef as ImplDef, Signature as Signature, impl as impl
 from .frame import Frame as Frame, FrameABC as FrameABC
